237756d821a09323c37251780396a4c05b2eaf22
Alexey <alex.erm@yandex.ru>
2018-12-28 15:38:18 +0300

    fix html-element-collection/drum-machine/script.js

--- html-element-collection/drum-machine/script.js
+++ html-element-collection/drum-machine/script.js
@@ -2,9 +2,12 @@
 const drumKit = document.getElementsByClassName('drum-kit__drum');
 
 for (let button of drumKit) {
- let audio = button.getElementsByTagName('audio')[0];
- 
- button.onclick = function() {
-  audio.play();
- }
+  let audio = button.getElementsByTagName('audio')[0];
+
+  button.onclick = function() {
+    audio.pause();
+    audio.currentTime = 0;
+
+    audio.play();
+  }
 }
