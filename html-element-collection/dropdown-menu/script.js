54b4aa7b3a24a256286be02ee528b8e164b83b1f
Alexey <alex.erm@yandex.ru>
2018-12-28 15:30:24 +0300

    fix html-element-collection/dropdown/script.js

--- html-element-collection/dropdown-menu/script.js
+++ html-element-collection/dropdown-menu/script.js
@@ -1,14 +1,6 @@
 "use strict"
 const wrapperDropdown = document.getElementsByClassName('wrapper-dropdown')[0];
-console.log(wrapperDropdown.overflow)
 
-let toggle = 1;
 wrapperDropdown.onclick = function() {
-  if (toggle === 1) {
-    wrapperDropdown.classList.add('active');
-    toggle = 0;
-  } else {
-    wrapperDropdown.classList.remove('active');
-    toggle = 1;
-  }
+  wrapperDropdown.classList.toggle('active');
 }
