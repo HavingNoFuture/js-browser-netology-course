8a5dabe19f61602bcc49ef140162cb2f8f0ff805
Alexey <alex.erm@yandex.ru>
2019-01-11 14:51:10 +0300

    finish 2nd task in ./xhr

--- xhr/tabs/js/script.js
+++ xhr/tabs/js/script.js
@@ -1,3 +1,33 @@
 "use strict"
 
-const EmailBtn
+const tabs = document.querySelectorAll('.tabs nav a');
+const preloader = document.querySelector('#preloader');
+const content = document.querySelector('#content');
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('loadstart', () => {
+	preloader.classList.remove('hidden');
+});
+
+xhr.addEventListener('load', () => {
+	preloader.classList.add('hidden');
+	content.innerHTML = xhr.responseText;
+});
+
+function request(url) {
+	xhr.open('GET', url);
+	xhr.send();
+}
+
+function onClick(event) {
+	event.preventDefault();
+	tabs.forEach((tab) => tab.classList.remove('active'));
+	event.target.classList.add('active');
+	request(event.target.href);
+}
+
+for (let tab of tabs) {
+	tab.addEventListener('click', onClick);
+}
+
+request(tabs[0]);
