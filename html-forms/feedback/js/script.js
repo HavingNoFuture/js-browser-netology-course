6a635c1a434387db7a136f97329d4c645237ecff
Alexey <alex.erm@yandex.ru>
2019-01-15 12:47:38 +0300

    add 3rd task in ./html-forms

---
+++ html-forms/feedback/js/script.js
@@ -0,0 +1,35 @@
+"use strict"
+const contentForm = document.querySelector('.contentform');
+const inputList = contentForm.querySelectorAll('[name]');
+const btnSendMsg = contentForm.querySelector('.button-contact');
+const outputForm = document.querySelector('#output');
+const btnEditMsg = outputForm.querySelector('.button-contact');
+
+let empty = inputList.length;
+
+for (let input of inputList) {
+	if (input.name === 'zip') input.type = 'number';
+
+	input.addEventListener('change', onChange);
+	function onChange(event) {
+		this.value != '' ? empty-- : empty++;
+
+		if (empty === 0) {
+			btnSendMsg.removeAttribute('disabled');
+		} else {
+			btnSendMsg.setAttribute('disabled', '');
+		}
+
+		if ((this.name != 'phone') && (this.name != 'email')) {
+			const output = outputForm.querySelector(`#${this.name}`);
+			output.value = this.value;
+		}
+	}
+}
+
+btnSendMsg.addEventListener('click', onClick);
+btnEditMsg.addEventListener('click', onClick);
+function onClick() {
+	contentForm.classList.toggle('hidden');
+	outputForm.classList.toggle('hidden');
+}
