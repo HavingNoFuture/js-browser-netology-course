d4594a12b0c3a45e481a1295730c5f3aa91ebd6d
Alexey <alex.erm@yandex.ru>
2019-01-27 15:37:59 +0300

    fix 3rd task in ./html-document-structure

--- html-document-structure/tabs/js/tabs.js
+++ html-document-structure/tabs/js/tabs.js
@@ -5,14 +5,17 @@ const articles = tabForm.querySelectorAll('.tabs-content [data-tab-title]');
 
 tabNavForm.innerHTML = '';
 for (let article of articles) {
-	tabNavForm.innerHTML += `<li><a class="fa" data-tab-title=${article.dataset.tabTitle}>${article.dataset.tabTitle}</a></li>`;
+	tabNavForm.innerHTML += `<li><a class="fa ${article.dataset.tabIcon}" data-tab-title=${article.dataset.tabTitle}>${article.dataset.tabTitle}</a></li>`;
 }
 
 const tabs = tabNavForm.querySelectorAll('li');
 tabs[0].classList.add('ui-tabs-active');
 
-articles[1].classList.add('hidden');
-articles[2].classList.add('hidden');
+for (let article of articles) {
+	if (article != articles[0]) {
+		article.classList.add('hidden');
+	}
+}
 
 for (let tab of tabs) {
 	tab.addEventListener('click', onClick);
