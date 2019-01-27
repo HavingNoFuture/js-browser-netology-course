f92d6188caeaa9e9cf54b6c9a1e28c73ebc9dc04
Alexey <alex.erm@yandex.ru>
2019-01-27 15:26:01 +0300

    fix 2nd task in ./html-document-structure

--- html-document-structure/todo-list/js/todo.js
+++ html-document-structure/todo-list/js/todo.js
@@ -3,15 +3,15 @@ const doneList = document.querySelector(".done");
 const undoneList = document.querySelector(".undone");
 const labels = document.querySelectorAll(".todo-list label");
 
+function onClick(event) {
+	const input = label.querySelector('input');
+	label.parentNode.removeChild(label);
+	if (input.checked) {
+		doneList.appendChild(label);
+	} else {
+		undoneList.appendChild(label);
+	}
+}
 for (let label of labels) {
 	label.addEventListener('change', onClick);
-	function onClick(event) {
-		const input = label.querySelector('input');
-		label.parentNode.removeChild(label);
-		if (input.checked) {
-			doneList.appendChild(label);
-		} else {
-			undoneList.appendChild(label);
-		}
-	}
 }
