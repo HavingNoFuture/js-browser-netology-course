e16b941f2c6be4c9d07b8401d0385d6121ff76ac
Alexey <alex.erm@yandex.ru>
2019-02-05 22:03:47 +0300

    add 1st task in ./comet

---
+++ comet/realtime/js/realtime.js
@@ -0,0 +1,26 @@
+const ctx = document.getElementById('chart').getContext('2d');
+const realtime = new Chart(ctx).Bar({
+  labels: [],
+  datasets: [{
+    fillColor: 'rgba(0,60,100,1)',
+    strokeColor: 'black',
+    data: []
+  }]
+}, {
+  responsive: true,
+  barValueSpacing: 2
+});
+
+let isFirst = true;
+const ws = new WebSocket('wss://neto-api.herokuapp.com/realtime');
+ws.addEventListener('message', event => {
+  const dataObj = JSON.parse(event.data);
+
+  if (isFirst) {
+    dataObj.forEach(data => realtime.addData([Number(data['online'])], data['time']));
+    isFirst = false;
+  } else {
+    realtime.removeData();
+    realtime.addData([Number(dataObj['online'])], dataObj['time']);
+  }
+});
